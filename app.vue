dfaee5835fbcbeff1c1847828a9ef6bee17b542f
Ramon Wanderley <ramonwanderley13@gmail.com>
2022-02-27 10:13:22 -0300

    logic applied

--- app.vue
+++ app.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="center">
+   
+
+    
+    <div class="row">
+      <p>
+      Bruno vai ser pai?<br/>
+      </p>
+    </div>
+    <div class="row">
+      <p >
+        {{ !confirmed ? '50% de chance' : (isFather ? 'Sim' : 'Não') }}
+        
+      </p>
+    </div>
+    
+    
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      confirmed: false,
+      isFather: false, 
+    }
+  }
+}
+</script>
+<style>
+ .center {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   flex-wrap: nowrap;
+
+ }
+
+ .row {
+  width: 100vw;
+  flex-grow: 12;
+
+ }
+ .main-text {
+   color: brown;
+ }
+</style>
